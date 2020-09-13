f070fa655d59c389b38e93ec4e921c12b4e44cea
RenanCostaa <renancosta@ucl.br>
2020-09-13 22:44:36 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+/*Crie uma classe que modele uma pessoa:
+
+Atributos: nome, idade, peso e altura
+Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
+*/
+
+class MainClass {
+  public static void Main () {
+
+  }
+}
+
+class Pessoa{
+  string nome;
+  int idade;
+  float peso;
+  float altura;
+
+
+  public envelhecer(){
+
+  }
+  public engordar(){
+    
+  }
+}
